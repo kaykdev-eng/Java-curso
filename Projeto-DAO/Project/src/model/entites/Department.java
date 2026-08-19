f74ce049d788798d0f682a1bcfa4b70060d7670f
Kayk magalhaes <kaykapple1@gmail.com>
2026-08-18 21:40:22 -0300

    DepartmentDao and SellerDao interfaces

--- Projeto-DAO/Project/src/model/entites/Department.java
+++ Projeto-DAO/Project/src/model/entites/Department.java
@@ -1,4 +1,4 @@
-package entites;
+package model.entites;
 
 import java.io.Serializable;
 import java.util.Objects;
--- Projeto-DAO/Project/src/model/entites/Seller.java
+++ Projeto-DAO/Project/src/model/entites/Seller.java
@@ -1,4 +1,4 @@
-package entites;
+package model.entites;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class Seller implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     private Integer id;
     private String name;
     private String email;
